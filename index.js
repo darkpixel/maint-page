54c73ffba6d1541712e4b5e4a5a6aa13e7fe8803
Aaron C. de Bruyn <aaron@heyaaron.com>
2022-12-20 14:02:33 -0800

    Attempt to enable PROXY support

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var http = require( 'http' )
+var proxiedHttp = require( 'findhit-proxywrap' ).proxy( http )
 var morgan = require('morgan')
 const express = require('express')
 const app = express()
 const port = 3000
+
+var srv = proxiedHttp.createServer(app)
 
 app.use(morgan('combined'))
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
@@ -9,6 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+srv.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
