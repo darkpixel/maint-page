de9bb20748a4bf011e945987a40eeafa5e18168b
Aaron C. de Bruyn <aaron@heyaaron.com>
2022-12-20 16:01:31 -0800

    Tweaks

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var srv = proxiedHttp.createServer(app)
-
 app.use(morgan('combined'))
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
 app.get('*', (req, res) => {
   res.send('Hello World!')
 })
 
+var srv = proxiedHttp.createServer(app)
 srv.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
